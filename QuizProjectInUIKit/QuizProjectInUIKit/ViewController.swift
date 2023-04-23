e23b1f19e96c390da4f9c97daca425ad8e91f767
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-04-23 17:16:30 +0900

    QuizProject UIKit commit

---
+++ QuizProjectInUIKit/QuizProjectInUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizProjectInUIKit
+//
+//  Created by yosshi on 2023/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
