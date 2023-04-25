88adda0ccb48d98d799cf7eab7d31be6ed137c4a
yosshi0127-ios <yosshi.0127.ios@gmail.com>
2023-04-26 08:35:45 +0900

    SwiftUI プロジェクト追加

---
+++ QuizProjectInSwiftUI/Pods/Target Support Files/Pods-QuizProjectInSwiftUI-QuizProjectInSwiftUIUITests/Pods-QuizProjectInSwiftUI-QuizProjectInSwiftUIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuizProjectInSwiftUI_QuizProjectInSwiftUIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_QuizProjectInSwiftUI_QuizProjectInSwiftUIUITests
+@end
---
+++ QuizProjectInSwiftUI/QuizProjectInSwiftUI/QuizProjectInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizProjectInSwiftUIApp.swift
+//  QuizProjectInSwiftUI
+//
+//  Created by yosshi on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct QuizProjectInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
